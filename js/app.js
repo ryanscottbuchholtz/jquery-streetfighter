67d7bc4cc3b0d1fe6ed0416b3eda26242594ba33
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-24 19:32:41 -0700

    main css js code show/hide complete

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function(){
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  }).mouseleave(function(){
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+  });
+  $('.ryu').mousedown(function(){
+    playHadouken();
+    $('.ryu-still').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
+  });
+    $('.ryu').mouseup(function(){
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+  });
+
+});
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
