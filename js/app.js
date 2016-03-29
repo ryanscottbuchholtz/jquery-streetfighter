b9fd8c388b4563113a832ad12f1fdd7781c8ce9f
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-29 09:17:17 -0700

    refactor show/hide function js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
   
   $('.ryu').mouseenter(function(){
-    $('.ryu-cool').hide();
-    $('.ryu-still').hide();
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  
+    // $('.ryu-cool').hide();
+    // $('.ryu-still').hide();
+    // $('.ryu-throwing').hide();
+    // $('.ryu-ready').show();
+    showThisImage('ryu-ready', 'ryu2');
+
   }).mouseleave(function(){
-    $('.ryu-cool').hide();
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
+    // $('.ryu-cool').hide();
+    // $('.ryu-throwing').hide();
+    // $('.ryu-ready').hide();
+    // $('.ryu-still').show();
+    showThisImage('ryu-still', 'ryu2');
   });
 
   $('.ryu').mousedown(function(){
     playHadouken();
-    $('.ryu-still').hide();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
+    // $('.ryu-still').hide();
+    // $('.ryu-ready').hide();
+    // $('.ryu-throwing').show();
+    showThisImage('ryu-throwing', 'ryu2');
     $('.hadouken').finish().show().animate(
       {'left': '2400px'},
       1000,
@@ -29,27 +32,30 @@ $(document).ready(function() {
   });
 
   $('.ryu').mouseup(function(){
-    $('.ryu-still').show();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').hide();
+    // $('.ryu-still').show();
+    // $('.ryu-ready').hide();
+    // $('.ryu-throwing').hide();
+    showThisImage('ryu-still', 'ryu2');
   });
 
   $(document).keydown(function(e){
     if(e.keyCode === 88) {
       document.body.style.pointerEvents='none';
-      $('.ryu-cool').show();
-      $('.ryu-ready').hide();
-      $('.ryu-still').hide();
-      $('.ryu-throwing').hide();
+      // $('.ryu-cool').show();
+      // $('.ryu-ready').hide();
+      // $('.ryu-still').hide();
+      // $('.ryu-throwing').hide();
+      showThisImage('ryu-cool', 'ryu2');
     }
   });
   $(document).keyup(function(e){
     if(e.keyCode === 88) {
       document.body.style.pointerEvents='auto';
-      $('.ryu-still').show();
-      $('.ryu-throwing').hide();
-      $('.ryu-cool').hide();
-      $('.ryu-ready').hide();
+      // $('.ryu-still').show();
+      // $('.ryu-throwing').hide();
+      // $('.ryu-cool').hide();
+      // $('.ryu-ready').hide();
+      showThisImage('ryu-ready', 'ryu2');
     }
   });
 
@@ -61,3 +67,21 @@ function playHadouken() {
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+
+function showThisImage(imageClassNameToShow, imageClassNameToHide) {
+  // 1) define two variables to capture elements to control
+  var hiddenImages = document.getElementsByClassName(imageClassNameToHide);
+  var visibleImage = document.getElementsByClassName(imageClassNameToShow);
+  // 2) hide all images based on imageClassNameToHide
+  $(hiddenImages).hide();
+  // 3) show specific image based on imageClassName
+  $(visibleImage).show();
+
+}
+
+
+
+
+
+
